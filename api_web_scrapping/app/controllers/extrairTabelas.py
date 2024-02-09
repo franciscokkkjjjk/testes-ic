dc8970920620ec101d56ed3b8ec569caaa513e7c
franciscokkkjjjk <luisfrancisco.15.brum@gmail.com>
2024-02-09 11:33:30 -0300

    Adicionando parametros opcionais de substituicao de palavras na tabela

--- api_web_scrapping/app/controllers/extrairTabelas.py
+++ api_web_scrapping/app/controllers/extrairTabelas.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Dict, Union, List
 from fastapi import APIRouter
 from models.arquivo import Arquivo
 from pydantic import BaseModel
@@ -8,10 +8,10 @@ routerED = APIRouter()
 
 class paramsSubstituicao(BaseModel):
     pesquisa: str
-    substitui: str 
+    substitui: str
 
 @routerED.get('/extrairTabelas/{nome_arquivo}')
-async def extrairDados(nome_arquivo, paginas=None, paramsSubs: paramsSubstituicao = None):
+async def extrairDados(nome_arquivo, paginas=None, paramsSubs: List[paramsSubstituicao] | paramsSubstituicao | None  = None):
     arq = Arquivo()
     lista_de_tabelas = arq.ler_pdf(nome_pdf=nome_arquivo, paginas=paginas)
     if len(lista_de_tabelas) > 0:
@@ -19,7 +19,14 @@ async def extrairDados(nome_arquivo, paginas=None, paramsSubs: paramsSubstituica
     if len(lista_de_tabelas) == 0:
         return {
             'mensagem': 'Nenhuma tabela foi encontrada.'
-        }   
-    print(lista_de_tabelas)
+        }
+        
+    if(paramsSubs):
+            if(isinstance(paramsSubs, list)):
+                for param in paramsSubs:
+                    lista_de_tabelas = lista_de_tabelas.replace(param.pesquisa, param.substitui)
+            else:
+                lista_de_tabelas = lista_de_tabelas.replace(paramsSubs.pesquisa, paramsSubs.substitui)
+                
     arq.converte_csv(nome_arquivo, lista_de_tabelas)
     return {True}
--- api_web_scrapping/app/rotas/anexo.py
+++ api_web_scrapping/app/rotas/anexo.py
@@ -1,8 +1,7 @@
 from fastapi import APIRouter
-from controllers import downloadAnexo, extrairTabelas, substituirNomeclaturaTabela 
+from controllers import downloadAnexo, extrairTabelas 
 
 router = APIRouter()
 prefixo = '/anexo'
 router.include_router(router=downloadAnexo.routerDW, prefix=prefixo)
 router.include_router(router=extrairTabelas.routerED, prefix=prefixo)
-router.include_router(router=substituirNomeclaturaTabela.routerSNT, prefix=prefixo)
