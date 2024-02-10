9cd0883382802121e1a12183facf9b85fe46b6ee
Luis Francisco <luisfrancisco.15.brum@gmail.com>
2024-02-10 19:54:36 -0300

    Fix: import de csv do relatorio cadop

--- teste_banco_dados/importa_dados_relatorio_cadop_csv.sql
+++ teste_banco_dados/importa_dados_relatorio_cadop_csv.sql
@@ -2,7 +2,7 @@ use `dados_cadastrais`;
 LOAD DATA LOCAL INFILE '-1'
 INTO TABLE dados_cadastrais_das_operadoras_ativas_ans
 FIELDS TERMINATED BY ';'
-ENCLOSED by '"'
-LINES TERMINATED BY '\n'
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS;
 
