c75fc90481ba2d335b59fddf005a1045dd4ccc74
Luis Francisco <luisfrancisco.15.brum@gmail.com>
2024-02-10 19:53:44 -0300

    Fix: import de arquivo de 2023

--- teste_banco_dados/importa_dados_2023.sql
+++ teste_banco_dados/importa_dados_2023.sql
@@ -2,22 +2,48 @@ use `dados_cadastrais`;
 
 LOAD DATA LOCAL INFILE '-1'
 INTO TABLE demonstracoes_contabeis
+CHARACTER SET utf8
 FIELDS TERMINATED BY ';'
-ENCLOSED by '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS
+(@coluna1,@coluna2,@coluna3,@coluna4,@var_col_inicial, @var_col_final)
+SET 
+    DATA = @coluna1,
+    REG_ANS = @coluna2,
+    CD_CONTA_CONTABIL = @coluna3,
+    DESCRICAO = @coluna4,
+    VL_SALDO_INICIAL = REPLACE(REPLACE(@var_col_inicial, '.', ''), ',', '.'),
+    VL_SALDO_FINAL = REPLACE(REPLACE(@var_col_inicial, '.', ''), ',', '.');
 
-
-LOAD DATA LOCAL INFILE '-2' 
+LOAD DATA LOCAL INFILE '-2'
 INTO TABLE demonstracoes_contabeis
+CHARACTER SET utf8
 FIELDS TERMINATED BY ';'
-ENCLOSED by '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS
+(@coluna1,@coluna2,@coluna3,@coluna4,@var_col_inicial, @var_col_final)
+SET 
+    DATA = @coluna1,
+    REG_ANS = @coluna2,
+    CD_CONTA_CONTABIL = @coluna3,
+    DESCRICAO = @coluna4,
+    VL_SALDO_INICIAL = REPLACE(REPLACE(@var_col_inicial, '.', ''), ',', '.'),
+    VL_SALDO_FINAL = REPLACE(REPLACE(@var_col_inicial, '.', ''), ',', '.');
 
-LOAD DATA LOCAL INFILE '-3' 
+LOAD DATA LOCAL INFILE '-3'
 INTO TABLE demonstracoes_contabeis
+CHARACTER SET utf8
 FIELDS TERMINATED BY ';'
-ENCLOSED by '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS;
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 ROWS
+(@coluna1,@coluna2,@coluna3,@coluna4,@var_col_inicial, @var_col_final)
+SET 
+    DATA = @coluna1,
+    REG_ANS = @coluna2,
+    CD_CONTA_CONTABIL = @coluna3,
+    DESCRICAO = @coluna4,
+    VL_SALDO_INICIAL = REPLACE(REPLACE(@var_col_inicial, '.', ''), ',', '.'),
+    VL_SALDO_FINAL = REPLACE(REPLACE(@var_col_inicial, '.', ''), ',', '.');
