3c8e2e1f24da5770c5a6a55a5308da006f607e44
franciscokkkjjjk <luisfrancisco.15.brum@gmail.com>
2024-02-10 11:45:26 -0300

    Implementação de um script para o caminho absoluto dinamico

---
+++ teste_banco_dados/gera_caminho_absoluto_sql.py
@@ -0,0 +1,12 @@
+import os
+
+arquivo_csv_path = os.path.abspath('.') + '/arquivos/Relatorio_cadop.csv'
+arquivo_sql_path = os.path.abspath('.') + '/importa_dados_csv.sql'
+with open(arquivo_sql_path, 'r') as f:
+    conteudo_sql = f.read()
+    diretorio_anterior = conteudo_sql.split('\'')[1]
+    conteudo_substituido = conteudo_sql.replace(diretorio_anterior, arquivo_csv_path)
+    os.remove(arquivo_sql_path)
+    
+with open(arquivo_sql_path, 'w') as f:
+    f.write(conteudo_substituido)
