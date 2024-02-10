d0a05b8d609ad41a03e3be2f4ccee279e80ff153
Luis Francisco <luisfrancisco.15.brum@gmail.com>
2024-02-10 12:13:02 -0300

    FIX: importação de arquivos

--- teste_banco_dados/importa_dados_csv.sql
+++ teste_banco_dados/importa_dados_csv.sql
@@ -1,5 +1,5 @@
 use `dados_cadastrais`;
-LOAD DATA INFILE '/home/francisco/projetos/python/testes-ic/teste_banco_dados/arquivos/Relatorio_cadop.csv'
+LOAD DATA LOCAL INFILE 'C:\Users\luisf\projetos\python\testes-ic\teste_banco_dados\arquivos\Relatorio_cadop.csv'
 INTO TABLE dados_cadastrais_das_operadoras_ativas_ans
 FIELDS TERMINATED BY ';'
 ENCLOSED by '"'
