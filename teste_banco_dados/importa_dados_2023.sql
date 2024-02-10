29556bae2529933dd9c74a35e6ad6a33867e9d3f
Luis Francisco <luisfrancisco.15.brum@gmail.com>
2024-02-10 14:07:32 -0300

    FIX: nome das tabelas

--- teste_banco_dados/importa_dados_2023.sql
+++ teste_banco_dados/importa_dados_2023.sql
@@ -1,7 +1,7 @@
 use `dados_cadastrais`;
 
 LOAD DATA LOCAL INFILE '-1'
-INTO TABLE dados_cadastrais_das_operadoras_ativas_ans
+INTO TABLE demonstracoes_contabeis
 FIELDS TERMINATED BY ';'
 ENCLOSED by '"'
 LINES TERMINATED BY '\n'
@@ -9,14 +9,14 @@ IGNORE 1 ROWS;
 
 
 LOAD DATA LOCAL INFILE '-2' 
-INTO TABLE dados_cadastrais_das_operadoras_ativas_ans
+INTO TABLE demonstracoes_contabeis
 FIELDS TERMINATED BY ';'
 ENCLOSED by '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
 LOAD DATA LOCAL INFILE '-3' 
-INTO TABLE dados_cadastrais_das_operadoras_ativas_ans
+INTO TABLE demonstracoes_contabeis
 FIELDS TERMINATED BY ';'
 ENCLOSED by '"'
 LINES TERMINATED BY '\n'
