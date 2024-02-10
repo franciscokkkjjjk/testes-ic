478e667f54e0128f211d73d2e2353e2e0f180ca2
Luis Francisco <luisfrancisco.15.brum@gmail.com>
2024-02-10 19:45:34 -0300

    Altera criação da tabela de demostracoes contabeis

--- teste_banco_dados/create_table_demonstracoes_contabeis.sql
+++ teste_banco_dados/create_table_demonstracoes_contabeis.sql
@@ -1,9 +1,9 @@
-CREATE TABLE `dados_cadastrais`.`demonstracoes_contabeis` (
+use `dados_cadastrais`;
+CREATE TABLE `demonstracoes_contabeis` (
 	`data` DATE NOT NULL,
 	`reg_ans` integer NOT NULL,
 	`cd_conta_contabil` INT NOT NULL,
 	`descricao` VARCHAR(180) NOT NULL,
-	`vl_saldo_inicial` VARCHAR(25) NULL,
-	`vl_saldo_final` VARCHAR(25) NULL,
-    FOREIGN KEY (reg_ans) REFERENCES dados_cadastrais_das_operadoras_ativas_ans(Registro_ANS)
-) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+	`vl_saldo_inicial` FLOAT NOT NULL,
+	`vl_saldo_final` FLOAT NOT NULL
+) CHARACTER SET utf8mb4 COLLATE UTF8MB4_GENERAL_CI;
