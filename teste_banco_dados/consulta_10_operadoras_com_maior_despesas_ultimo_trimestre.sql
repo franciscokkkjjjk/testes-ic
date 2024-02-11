4a81405b718987c0bfb7f5347d08c76c7e294346
Luis Francisco <luisfrancisco.15.brum@gmail.com>
2024-02-11 18:11:21 -0300

    Implementando consulta das 10 operadoras com maior despesas do ultimo trimestre

---
+++ teste_banco_dados/consulta_10_operadoras_com_maior_despesas_ultimo_trimestre.sql
@@ -0,0 +1,8 @@
+SELECT *, (dc.vl_saldo_final - dc.vl_saldo_inicial) AS despesas
+FROM dados_cadastrais.demonstracoes_contabeis dc 
+INNER JOIN dados_cadastrais.dados_cadastrais_das_operadoras_ativas_ans dcoaa
+ON dc.reg_ans = dcoaa.Registro_ANS
+WHERE dc.`data` = '2023-07-01' 
+AND dc.descricao LIKE '%EVENTOS/ SINISTROS CONHECIDOS OU AVISADOS  DE ASSISTÊNCIA A SAÚDE MEDICO HOSPITALAR%'
+ORDER BY despesas ASC
+LIMIT 10
