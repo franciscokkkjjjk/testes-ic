73fd5af198549352074ee79da3e607493b13fefd
Luis Francisco <luisfrancisco.15.brum@gmail.com>
2024-02-11 17:50:51 -0300

    Preparando ambiente para desenvolvimento do item 4

---
+++ teste_api_vue/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    HEADER...
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped>
+  
+</style>
