ac920710aa1df28f0a1b831ce589577dbf82ffec
Luis Francisco <luisfrancisco.15.brum@gmail.com>
2024-02-11 17:56:22 -0300

    Fix: saldo inicial igual ao saldo final

--- teste_banco_dados/importa_dados_2023.sql
+++ teste_banco_dados/importa_dados_2023.sql
@@ -2,7 +2,7 @@ use `dados_cadastrais`;
 
 LOAD DATA LOCAL INFILE '-1'
 INTO TABLE demonstracoes_contabeis
-CHARACTER SET utf8
+CHARACTER SET utf8mb4
 FIELDS TERMINATED BY ';'
 ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
@@ -14,11 +14,11 @@ SET
     CD_CONTA_CONTABIL = @coluna3,
     DESCRICAO = @coluna4,
     VL_SALDO_INICIAL = REPLACE(REPLACE(@var_col_inicial, '.', ''), ',', '.'),
-    VL_SALDO_FINAL = REPLACE(REPLACE(@var_col_inicial, '.', ''), ',', '.');
+    VL_SALDO_FINAL = REPLACE(REPLACE(@var_col_final, '.', ''), ',', '.');
 
 LOAD DATA LOCAL INFILE '-2'
 INTO TABLE demonstracoes_contabeis
-CHARACTER SET utf8
+CHARACTER SET utf8mb4
 FIELDS TERMINATED BY ';'
 ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
@@ -30,11 +30,11 @@ SET
     CD_CONTA_CONTABIL = @coluna3,
     DESCRICAO = @coluna4,
     VL_SALDO_INICIAL = REPLACE(REPLACE(@var_col_inicial, '.', ''), ',', '.'),
-    VL_SALDO_FINAL = REPLACE(REPLACE(@var_col_inicial, '.', ''), ',', '.');
+    VL_SALDO_FINAL = REPLACE(REPLACE(@var_col_final, '.', ''), ',', '.');
 
 LOAD DATA LOCAL INFILE '-3'
 INTO TABLE demonstracoes_contabeis
-CHARACTER SET utf8
+CHARACTER SET utf8mb4
 FIELDS TERMINATED BY ';'
 ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
@@ -46,4 +46,4 @@ SET
     CD_CONTA_CONTABIL = @coluna3,
     DESCRICAO = @coluna4,
     VL_SALDO_INICIAL = REPLACE(REPLACE(@var_col_inicial, '.', ''), ',', '.'),
-    VL_SALDO_FINAL = REPLACE(REPLACE(@var_col_inicial, '.', ''), ',', '.');
+    VL_SALDO_FINAL = REPLACE(REPLACE(@var_col_final, '.', ''), ',', '.');
