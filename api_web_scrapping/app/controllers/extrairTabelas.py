e369fab9c7343df404a51e95ced9e784d6484d6a
Luis Francisco <luisfrancisco.15.brum@gmail.com>
2024-02-11 22:49:16 -0300

    implementando tratamento de erro caso o arquivo não seja encontrado para extração

--- api_web_scrapping/app/controllers/extrairTabelas.py
+++ api_web_scrapping/app/controllers/extrairTabelas.py
@@ -5,16 +5,23 @@ import pandas
 
 routerED = APIRouter()
 
-@routerED.get('/extrairTabelas/{nome_arquivo}')
-async def extrairDados(nome_arquivo, paginas=None):
+@routerED.get('/extrairTabelas/')
+async def extrairDados(paginas=None):
     arq = Arquivo()
-    lista_de_tabelas = arq.ler_pdf(nome_pdf=nome_arquivo, paginas=paginas)
-    
+    nome_arquivo = 'Anexo_I_Rol_2021RN_465.2021_RN592'
+    try:
+        lista_de_tabelas = arq.ler_pdf(nome_pdf=nome_arquivo, paginas=paginas)
+    except FileNotFoundError:
+        return {
+            'mensagem': f'Arquivo {nome_arquivo} não encontrado.',
+            'status': False 
+        }
     if len(lista_de_tabelas) > 0:
         lista_de_tabelas = pandas.concat(lista_de_tabelas)
     if len(lista_de_tabelas) == 0:
         return {
-            'mensagem': 'Nenhuma tabela foi encontrada.'
+            'mensagem': 'Nenhuma tabela foi encontrada.',
+            'status': False
         }
         
     pdf_text = arq.ler_pdf_text(nome_pdf=nome_arquivo, num_maximo_paginas=3)
@@ -31,5 +38,5 @@ async def extrairDados(nome_arquivo, paginas=None):
     caminho_absoluto_csv = arq.converte_csv(nome_arquivo, lista_de_tabelas)
     arq.compactar(caminho_absoluto_csv, 'Teste_luis_francisco_brum_gomes')
     return {
-        "status_anexo": True
+        "status": True
     }
